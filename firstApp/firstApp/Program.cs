050c3010a2a254f20bada2a6a26dfe511341e8d3
10Kaiser <muzammilnaeem12@gmail.com>
2025-05-05 12:38:02 +0500

    Add files via upload

---
+++ firstApp/firstApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Xml.Linq;
+namespace firstApp
+{
+  class program
+    {
+        static void Main(string[] args)
+        {
+            string name = "STest1";
+            const string kabuto = "ライダーキック";
+            int age = 6;
+            int n1 = 1, n2 = 2, n3 = 3;
+            string fivefivefive = "Complete";
+            char faiz = 'φ';
+                
+
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine();
+            Console.WriteLine(n1); Console.WriteLine(n2); Console.WriteLine(n3);
+            Console.WriteLine(kabuto);
+            Console.WriteLine(fivefivefive);
+            Console.WriteLine(faiz);
+
+
+
+            string h1, h2 = h1 = h2 = "hopper";
+            Console.WriteLine(h1);
+
+            double point = 3.1415;
+            bool btest;
+            btest= false;
+            Console.WriteLine(btest);
+            Console.WriteLine(point);
+            Console.WriteLine(n1+n2+n3);
+
+        }
+    }
+}
