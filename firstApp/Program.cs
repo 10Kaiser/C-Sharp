8caba654af705b8cc221f02be46cad6270c022f6
10Kaiser <muzammilnaeem12@gmail.com>
2025-05-07 12:32:25 +0500

    Add files via upload
    7/5/25

---
+++ firstApp/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+namespace firstApp
+{
+  class program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "STest1";
+            //const string kabuto = "ライダーキック";
+            //int age = 6;
+            //int n1 = 1, n2 = 2, n3 = 3;
+            //string fivefivefive = "Complete";
+            //char faiz = 'φ';
+
+
+            //Console.WriteLine(name);
+            //Console.WriteLine(age);
+            //Console.WriteLine();
+            //Console.WriteLine(n1); Console.WriteLine(n2); Console.WriteLine(n3);
+            //Console.WriteLine(kabuto);
+            //Console.WriteLine(fivefivefive);
+            //Console.WriteLine(faiz);
+
+
+
+            //string h1, h2 = h1 = h2 = "hopper";
+            //Console.WriteLine(h1);
+
+            //double point = 3.1415;
+            //bool btest;
+            //btest = false;
+            //Console.WriteLine(btest);
+            //Console.WriteLine(point);
+            //Console.WriteLine(n1 + n2 + n3);
+
+
+            //int mynum = 35;
+            //Console.WriteLine(mynum);
+            //Console.WriteLine(mynum.GetType());
+            //double mydub = 64.5;
+            //Console.WriteLine(mydub);
+            //Console.WriteLine(mydub.GetType());
+            //int mynum2 = (int)mydub;
+            //Console.WriteLine(mynum2);
+
+            //string num6 = "32790";
+            //string num9 = "1";
+
+            //Console.WriteLine(Convert.ToInt32(num6) - Convert.ToInt32(num9));
+            //Console.WriteLine(int.Parse(num6) + int.Parse(num9));
+
+            //Console.WriteLine("Enter Name :");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Hi,My name is " + name);
+            //int n1 = Convert.ToInt32(Console.ReadLine());
+            //int n2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(n1 + n2);
+
+            int x=2;
+            x += 2;
+            Console.WriteLine(x);
+            x *= 4;
+            Console.WriteLine(x);
+            x -= 2;
+            Console.WriteLine(x);
+            x &= 40;
+            Console.WriteLine(x);
+            x ^= 2;
+            Console.WriteLine(x);
+            x /= 2;
+            Console.WriteLine(x);
+
+
+
+
+        }
+    }
+}
