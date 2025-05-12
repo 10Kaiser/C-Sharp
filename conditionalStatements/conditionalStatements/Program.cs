ae0e35ae3e2dda51c13e657935edcb28f9e4fb53
10Kaiser <muzammilnaeem12@gmail.com>
2025-05-12 12:38:15 +0500

    Add files via upload

---
+++ conditionalStatements/conditionalStatements/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conditionalStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("φ");
+            int Code = Convert.ToInt32(Console.ReadLine());
+            if (Code == 555)
+            {
+                Console.WriteLine("Complete");
+            }
+            else if (Code == 913)
+            {
+                Console.WriteLine("Complete");
+            } else
+            {
+                Console.WriteLine("Error");
+            }
+
+
+
+
+
+            //Console.WriteLine("Eter Age");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter Education Level");
+            //string edu = Console.ReadLine().ToLower();
+            ////if(age >= 18)
+            ////{
+            ////    Console.WriteLine("Complete");
+            ////} else {
+            ////    Console.WriteLine("Error");
+            ////}
+            //if (age >= 18)
+            //{
+            //    Console.WriteLine("Complete");
+            //} else if(edu == "inter")
+            //{
+            //    Console.WriteLine("Complete");
+            //} else
+            //{
+            //    Console.WriteLine("Error");
+            //}
+
+
+            //Console.WriteLine("Enter Email");
+            //string email = Console.ReadLine().ToLower();
+            //if (email == "admin@gmail.com") 
+            //{
+            //    Console.WriteLine("Enter Password");
+            //    string pass = Console.ReadLine().ToLower(); 
+            //    if (pass == "admin")
+            //    {
+            //        Console.WriteLine("Welcome");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Wrong Password");
+            //    }
+            //}
+            //else
+            //{
+
+            //    Console.WriteLine("Inccorect Email");
+            //}
+
+
+
+
+            //Console.WriteLine("Enter Day number ");
+            //int day =int.Parse(Console.ReadLine());
+            //switch(day)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Today is Sunday");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Today is Monday");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Today is Tuesday");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Today is Wednesday");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Today is Thursday");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Today is Friday in California");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Today is Saturday");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Enter a Number from 1-7");
+            //        break;
+
+
+            //}
+
+
+
+
+        }
+    }
+}
